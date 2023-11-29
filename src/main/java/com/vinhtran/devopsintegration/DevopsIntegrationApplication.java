11dcf53ba283ef1e33c986da0c9af6f5dce27d18
VinhTran <vinh.tran@waverleysoftware.com>
2023-11-30 00:37:12 +0700

    init project

---
+++ src/main/java/com/vinhtran/devopsintegration/DevopsIntegrationApplication.java
@@ -0,0 +1,18 @@
+package com.vinhtran.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication {
+
+    @GetMapping
+    public String message(){return "welcome to java";}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
+
+}
